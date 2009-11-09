17457afb30b94e93aa5a5de36a5e1ff99f0aca4c
Marcel Jackwerth <marcel@northdocks.com>
2009-11-09 15:11:56 +0100

    add first draft of schema

---
+++ test/schema.rb
@@ -0,0 +1,25 @@
+ActiveRecord::Schema.define(:version => 0) do
+  create_table :posts, :force => true do |t|
+    t.string :topic
+    t.text :content
+    t.integer :author_id
+    t.timestamps
+  end
+  create_table :versions, :force => true do |t|
+    t.integer :target_id
+    t.string :target_type
+    t.integer :author_id
+    t.string :author_type
+    t.integer :version
+    t.text :cause
+    t.timestamps
+  end
+  create_table :attribute_changes, :force => true do |t|
+    t.integer :version_id
+    t.string :attribute
+    t.string :old_class
+    t.text :old_value
+    t.string :new_class
+    t.text :new_value
+  end
+end
