fd33d6fc46b6a7e5ff44819d63fad592f2da4c84
Marcel Jackwerth <marcel@northdocks.com>
2009-11-09 17:54:49 +0100

    added models for versions/changes

---
+++ lib/models/attribute_change.rb
@@ -0,0 +1,27 @@
+class AttributeChange < ActiveRecord::Base
+  set_table_name :attribute_changes
+  
+  validates_presence_of :version
+  validates_uniqueness_of :attribute, :scope => :version_id
+  
+  belongs_to :version
+  
+  def update_by_diff(diff)
+    if new_record?
+      self.old_value = diff[0]
+      self.old_class = diff[0].class.name
+    end
+    
+    self.new_value = diff[1]
+    self.new_class = diff[1].class.name
+  end
+  
+  def old
+    old_value	   
+  end
+  
+  def new
+    new_value	   
+  end
+  
+end
---
+++ lib/models/version.rb
@@ -0,0 +1,30 @@
+class Version < ActiveRecord::Base
+  set_table_name :versions
+
+  named_scope :most_recent, :order => "created_at DESC"
+  
+  validates_presence_of :target
+  validates_presence_of :version
+  validates_numericality_of :version, :greater_than => 0
+  validates_uniqueness_of :version, :scope => [:target_type, :target_id]
+  
+  belongs_to :target, :polymorphic => true
+  belongs_to :author, :polymorphic => true
+  
+  has_many :attribute_changes
+  
+  before_validation_on_create do |model|
+    if model.target
+      model.version = model.target.new_record? ? 1 : (model.target.versions.maximum(:version) || 0) + 1
+    end
+  end
+  
+  def merge!(changes)
+    raise "cannot merge! a new_record" if new_record?
+    changes.collect do |attribute, diff|
+      change = attribute_changes.find_or_initialize_by_attribute(attribute.to_s)
+      change.update_by_diff(diff)
+      change.save!
+    end
+  end
+end
