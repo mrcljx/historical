8078ab73f2817757599519fb62fd7654df031ec5
Marcel Jackwerth <marcel@northdocks.com>
2010-08-14 12:50:31 +0200

    removed lib/ prefix from autoload (unnecessary)

--- lib/historical.rb
+++ lib/historical.rb
@@ -1,12 +1,12 @@
 module Historical
   IGNORED_ATTRIBUTES = [:id, :created_at, :updated_at]
   
-  autoload :ModelHistory, "lib/historical/model_history"
-  autoload :ActiveRecord, "lib/historical/active_record"
+  autoload :ModelHistory, "historical/model_history"
+  autoload :ActiveRecord, "historical/active_record"
   
   module Models
-    autoload :AttributeDiff,  'lib/historical/models/attribute_diff'
-    autoload :ModelDiff,      'lib/historical/models/model_diff'
-    autoload :ModelVersion,   'lib/historical/models/model_version'
+    autoload :AttributeDiff,  'historical/models/attribute_diff'
+    autoload :ModelDiff,      'historical/models/model_diff'
+    autoload :ModelVersion,   'historical/models/model_version'
   end
 end
