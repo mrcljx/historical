f20c74042840274d2404bc5b73812125bdab1fc0
Marcel Jackwerth <marcel@northdocks.com>
2010-09-07 10:21:48 +0200

    initializer should be run before rails eager_load of all models

--- lib/historical/railtie.rb
+++ lib/historical/railtie.rb
@@ -3,7 +3,7 @@ require 'rails'
 
 module Historical
   class Railtie < Rails::Railtie
-    config.after_initialize do
+    initializer "historical.attach_to_active_record" do
       ::ActiveRecord::Base.send(:extend, Historical::ActiveRecord)
     end
   end
