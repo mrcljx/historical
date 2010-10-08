fad0284ddf0a7e0c58cc91409839f83193706887
Marcel Jackwerth <marcel@northdocks.com>
2010-10-08 15:11:44 +0200

    added documentation

--- lib/historical/model_history.rb
+++ lib/historical/model_history.rb
@@ -21,51 +21,72 @@ module Historical
       end
     end
     
+    # Destroys the complete history (i.e. all versions) of this model. The model itself will remain untouched.
     def destroy
       versions.remove
       record.invalidate_history! if record.history == self
     end
   
+    # @return [Array<Models::ModelVersion>] All versions of this model (oldest first).
     def versions
       Models::ModelVersion.for_record(record).sort(:"meta.created_at".asc, :_id.asc)
     end
     
     delegate :version_index, :to => :own_version
     
+    # @return [Models::ModelVersion] The own version
     def own_version
       versions.skip(@base_version).limit(1).first.tap do |own|
         raise "couldn't find myself (base_version: #{@base_version}, versions: #{versions.count})" unless own
       end
     end
     
+    # @return [Models::ModelVersion] The immediate predecessor
     def previous_version
       own_version.previous
     end
     
+    # @return [Models::ModelVersion] The immediate successor
     def next_version
       own_version.next
     end
   
+    # @return [Models::ModelVersion] The latest version
     def latest_version
       versions.last
     end
   
+    # @return [Models::ModelVersion] The first (i.e. original) version
     def original_version
       versions.first
     end
   
+    # @return [Models::ModelVersion] The creation version (should be the {#original_version}).
     def creation
       versions.where("diff.diff_type" => "creation").first
     end
   
+    # @return [Models::ModelVersion] The update versions (every version except the {#creation}).
     def updates
       versions.where("diff.diff_type" => "update")
     end
     
+    # @param [Integer] position The index of the version to be found (zero-based).
+    # @return [Models::ModelVersion, nil] The found version or nil.
     def find_version(position)
       versions.skip(position).limit(1).first
     end
     
+    # Finds a version.
+    # @return [Models::ModelVersion, nil]
+    # @overload version_by_query(index)
+    #   Like {#find_version}.
+    #   @param [Integer] index The index of the version to be found (zero-based). 
+    #   @return [Models::ModelVersion]
+    # @overload version_by_query(relation)
+    #   Finds a relative version.
+    #   @param [:first, :creation, :latest, :next, :previous] relation The relation of the version to be found to the current version.
+    #   @return [Models::ModelVersion]
     def version_by_query(query)
       case query
       when Numeric                then find_version(query)
@@ -76,18 +97,24 @@ module Historical
       end
     end
     
+    # Restores a model.
+    # @param query A query like in {#version_by_query}
+    # @return [Object] A frozen instance of the model in it's earlier state.
     def restore(query)
       version = version_by_query(query)
       raise ::ActiveRecord::RecordNotFound, "version (base_version: #{base_version}, query: #{query}) does not exist" unless version
       version.restore(record)
     end
     
+    # Some helpers
     %w(original latest previous next).each do |k|
       alias_method k, "#{k}_version"
     end
     
     protected
     
+    # Spawns a creation version for this model.
+    # @private
     def spawn_creation!
       record.send(:spawn_version, :create).tap do |e|
         e.created_at = record.created_at
--- lib/historical/models/model_version.rb
+++ lib/historical/models/model_version.rb
@@ -28,38 +28,39 @@ module Historical::Models
     
     alias_method :record, :_record
   
-    # All other versions of the associated record
+    # @return [Array<ModelVersion>] All other versions of the associated record
     def siblings
       self.class.for_record(_record_id, _record_type)
     end
   
-    # All the previous versions (immediate predecessor first)
+    # @return [ModelVersion] All the previous versions (immediate predecessor first)
     def previous_versions
       (new? ? siblings : siblings.where(:"meta.created_at".lte => created_at, :_id.lt => _id)).sort(:"meta.created_at".desc, :_id.desc)
     end
   
-    # The immediate predecessor version
+    # @return [ModelVersion] The immediate predecessor version
     def previous
       previous_versions.first
     end
   
-    # All the next versions (immediate successor first)
+    # @return [Array<ModelVersion>] All the next versions (immediate successor first)
     def next_versions
       siblings.where(:"meta.created_at".gte => created_at, :_id.gt => _id).sort(:"meta.created_at".asc, :_id.asc)
     end
   
-    # The immediate successor version
+    # @return [ModelVersion] The immediate successor version
     def next
       next_versions.first
     end
     
-    # The current version index (zero-based)
+    # @return [Integer] The current version index (zero-based)
     def version_index
       previous_versions.count
     end
     
     # Restores the current version.
     # @param base A reference to the record (to reduce DB queries)
+    # @return [Object] The restored version.
     def restore(base = nil)
       base ||= record
       
@@ -78,7 +79,8 @@ module Historical::Models
       end
     end
     
-    # Prevents the restored version from being saved or modifed.
+    # Prevents the restored version from being saved or modifed
+    # @return [Object] A frozen instance of {#restore}
     def restore_with_protection(*args)
       restore_without_protection(*args).tap do |r|
         r.readonly!
@@ -87,9 +89,14 @@ module Historical::Models
     
     alias_method_chain :restore, :protection
     
-    # Return all versions for the provided record
-    # @param record_or_id [Record,Integer] The id of the record (or a record - then type can be left nil)
-    # @param type [String] The type of the record
+    # @return [Array<ModelVersion>] All versions for the provided record
+    # @overload for_record(id, type)
+    #   @param id [Integer] The id of the record
+    #   @param type [String] The type of the record
+    #   @return [Array<ModelVersion>]
+    # @overload for_record(record)
+    #   @param record [Object] The record itself
+    #   @return [Array<ModelVersion>]
     def self.for_record(record_or_id, type = nil)
       if type
         ModelVersion.where(:_record_id => record_or_id, :_record_type => type)
@@ -98,7 +105,7 @@ module Historical::Models
       end
     end
     
-    # Retrieve customized class definition for a record class (e.g. TopicVersion, MessageVersion)
+    # @return [Class] Customized class definition for a record class (e.g. TopicVersion, MessageVersion).
     def self.for_class(source_class)
       Historical::Models::Pool.pooled(Historical::Models::Pool.pooled_name(source_class, self)) do
         Class.new(self).tap do |cls|
